307ad0c3b446067f010f34d0cfb475b2ebb4d85b
Preisler <preisler.andras@gmail.com>
2023-02-18 23:22:29 +0100

    Update new.js

--- new.js
+++ new.js
@@ -1,19 +1,20 @@
 const fs = require('fs');
 
 const main = async(path) =>{
+
     const dir = await fs.promises.opendir(path);
 
     let list = []
-
     let file = await dir.read()
 
-    while (file != null) {
+
+    while (file.name != null) {
+
+        console.log(file.name);
         list.push(file.name)
         file = await dir.read()
-    }
-    console.log('====================================');
-    console.log(list);
-    console.log('====================================');
 
+    }
 }
+
 main('./')
